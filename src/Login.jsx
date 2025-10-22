a0c4360afcb576d0f8bfc8f4e6032b08a5592859
ssrawat1 <ssr911999@gmail.com>
2025-10-22 23:29:00 +0530

    Update the login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -111,7 +111,7 @@ const Login = () => {
       </form>
 
       <p className="text-center mt-3">
-        Don't have an account?{' '}
+        Don't have an account?
         <Link className="text-blue-600 hover:underline" to="/register">
           Register
         </Link>
