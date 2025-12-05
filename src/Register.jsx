2b84adae416b7d9f59b533dcc144cfb132360938
ssrawat1 <ssr911999@gmail.com>
2025-12-05 14:16:26 +0530

    fix Google Login button issue

--- src/Register.jsx
+++ src/Register.jsx
@@ -297,7 +297,7 @@ const Register = () => {
             {/* GitHub Login - Full Width */}
             <button
               onClick={handleGithubLogin}
-              className=" cursor-pointer max-w-[200px] flex items-center p-2 justify-center gap-3 border bg-black text-white border-gray-300 rounded-sm hover:bg-gray-700 hover:border-gray-300 transition-all duration-200 font-medium text-sm "
+              className=" cursor-pointer flex items-center p-2.5 justify-center gap-3 border bg-black text-white border-gray-300 rounded-sm hover:bg-gray-700 hover:border-gray-300 transition-all duration-200 font-medium text-sm "
             >
               <FaGithub className="text-xl" />
               <span>Continue with GitHub</span>
