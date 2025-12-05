bef05f70fedfac2e28401a626a56c8bd5a88a400
ssrawat1 <ssr911999@gmail.com>
2025-12-05 17:30:59 +0530

    prevent the users page from unauthorized access

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -384,7 +384,7 @@ function DirectoryView() {
                         d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                       />
                     </svg>
-                    <p className="text-xl font-semibold text-gray-400 mb-2">Empty Folder</p>
+                    <p className="text-xl text-gray-400 mb-2">Empty Folder</p>
                     <em className="text-gray-400">Upload files or create a folder to get started.</em>
                   </>
                 )}
