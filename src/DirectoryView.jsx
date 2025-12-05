c9ff0e6a9ffc6966f920c4b4760ce9fef6fe0bc6
ssrawat1 <ssr911999@gmail.com>
2025-12-05 17:20:52 +0530

    prevent the users page from unauthorized access

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -360,7 +360,7 @@ function DirectoryView() {
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        strokeWidth={2}
+                        strokeWidth={1}
                         d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                       />
                     </svg>
@@ -380,7 +380,7 @@ function DirectoryView() {
                       <path
                         strokeLinecap="round"
                         strokeLinejoin="round"
-                        strokeWidth={2}
+                        strokeWidth={1}
                         d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                       />
                     </svg>
