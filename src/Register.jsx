113b646f78d8c73710a7f0e9edba8c4d7d5f413a
ssrawat1 <ssr911999@gmail.com>
2025-12-06 01:04:56 +0530

    fixing breadcrumb fetaure

--- src/Register.jsx
+++ src/Register.jsx
@@ -226,19 +226,20 @@ const Register = () => {
             <div>
               <label className="block text-sm font-semibold text-gray-700 mb-2">Password</label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none"></div>
-                <input
-                  name="password"
-                  type={showPassword ? 'text' : 'password'}
-                  autoComplete="password"
-                  required
-                  placeholder="Create a strong password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg outline-none transition-all duration-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
-                <div onClick={() => setShowPassword(!showPassword)}>
-                  {showPassword ? <FaEye /> : <FaEyeSlash />}
+                <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
+                  <input
+                    name="password"
+                    type={showPassword ? 'text' : 'password'}
+                    autoComplete="password"
+                    required
+                    placeholder="Create a strong password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg outline-none transition-all duration-200 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  />
+                  <div onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? <FaEye /> : <FaEyeSlash />}
+                  </div>
                 </div>
               </div>
             </div>
