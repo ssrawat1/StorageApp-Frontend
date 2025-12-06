047b07c58972ce5c7492fd2ea97f43167feca86d
ssrawat1 <ssr911999@gmail.com>
2025-12-06 13:57:54 +0530

    fix the otp bug and showing error in the ui if email doesn't exit for while otp validation

--- src/components/DetailsPopup.jsx
+++ src/components/DetailsPopup.jsx
@@ -60,7 +60,7 @@ function DetailsPopup({ item, onClose }) {
           <div>
             <span className="font-semibold">Name:</span> {name}
           </div>
-          <div className='flex justify-center'>
+          <div className='flex justify-center items-center'>
             <span className="font-semibold">Path: </span>
             <div
               className="mt-1 max-w-full truncate text-gray-700"
