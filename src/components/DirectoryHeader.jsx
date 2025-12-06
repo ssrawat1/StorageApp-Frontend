61a6912c7331f851eadce0d3f5615d78f7b3cb23
ssrawat1 <ssr911999@gmail.com>
2025-12-06 11:01:40 +0530

    Add the divider between Login with Google and GitHub

--- src/components/DirectoryHeader.jsx
+++ src/components/DirectoryHeader.jsx
@@ -22,7 +22,7 @@ function DirectoryHeader({
   filesList,
 }) {
   const [showUserMenu, setShowUserMenu] = useState(false);
-  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
   const [userName, setUserName] = useState('Guest User');
   const [userEmail, setUserEmail] = useState('guest@example.com');
